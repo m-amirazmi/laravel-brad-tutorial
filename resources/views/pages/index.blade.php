1d326d459cb5a6abed8b613fd52b50d98cd8db43
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-02 10:16:30 +0800

    setting up the controller and blog stuff

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div class="jumbotron text-center">
+  <h1>Welcome To Laravel</h1>
+  <p>This is the laravel application from the "Laravel From Scratch Youtube Series"</p>
+  <p><a href="/login" class="btn btn-primary btn-lg" role="button">Login</a><a href="/register" role="button" class="btn btn-success btn-lg">Register</a></p>
+</div>
+@endsection
