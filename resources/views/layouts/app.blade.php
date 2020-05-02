099d0215b06db502b9da039e48ca5d69120a29db
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-02 11:26:13 +0800

    forms saving data and ckeditor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
         <title>{{config('app.name', 'LSAPP')}}</title>
     </head>
     <body>
       @include('inc.navbar')
       <div class="container">
+        @include('inc.messages')
         @yield('content')
       </div>
+      <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+      <script>
+          CKEDITOR.replace( 'article-ckeditor' );
+      </script>
     </body>
 </html>
