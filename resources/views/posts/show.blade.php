21173be22755b92ebe8ed5f3b3708f4318685606
Muhamad Amir <m.amirazmi@hotmail.com>
2020-05-02 11:39:46 +0800

    done with crud application

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,5 +7,10 @@
     <hr>
     <small>Written on {{$post->created_at}}</small>
     <hr>
-    <a href="/posts" class="btn btn-primary">Go Back</a>
+    <a href="/posts" class="btn btn-dark">Go Back</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-dark">Edit</a>
+    {!!Form::open(['action'=>['PostsController@destroy', $post->id], 'method'=>'POST', 'class'=>'pull-right'])!!}
+      {{Form::hidden('_method', 'DELETE')}}
+      {{Form::submit('Delete', ['class'=>'btn btn-danger'])}}
+    {!!Form::close()!!}
 @endsection
